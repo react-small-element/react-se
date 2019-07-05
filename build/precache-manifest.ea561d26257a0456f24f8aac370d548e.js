6bd9e56e44dac6de14eabab6039791cfd00fee46
zhangjinlong <1148786117@qq.com>
2019-07-05 13:47:25 +0800

    push build

---
+++ build/precache-manifest.ea561d26257a0456f24f8aac370d548e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26493786d4dcc848e5e454a7815a858",
+    "url": "./index.html"
+  },
+  {
+    "revision": "654bc12da58daf3b117a",
+    "url": "./static/css/main.9eb99fbf.chunk.css"
+  },
+  {
+    "revision": "648998961f8a536d92e9",
+    "url": "./static/js/2.d033b2f9.chunk.js"
+  },
+  {
+    "revision": "654bc12da58daf3b117a",
+    "url": "./static/js/main.c8c78eda.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
